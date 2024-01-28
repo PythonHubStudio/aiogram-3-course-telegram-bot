7810ef0378ff68ed625303564fd21d24eefd751c
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2024-01-28 14:29:32 +0200

    Для поверхностного ознакомления перед видео 6

---
+++ lesson_6-prepared_template_for_review/filters/chat_types.py
@@ -0,0 +1,18 @@
+from aiogram.filters import Filter
+from aiogram import Bot, types
+
+
+class ChatTypeFilter(Filter):
+    def __init__(self, chat_types: list[str]) -> None:
+        self.chat_types = chat_types
+
+    async def __call__(self, message: types.Message) -> bool:
+        return message.chat.type in self.chat_types
+    
+
+class IsAdmin(Filter):
+    def __init__(self) -> None:
+        pass
+
+    async def __call__(self, message: types.Message, bot: Bot) -> bool:
+        return message.from_user.id in bot.my_admins_list
