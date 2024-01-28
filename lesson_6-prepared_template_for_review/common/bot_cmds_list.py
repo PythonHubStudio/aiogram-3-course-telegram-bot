a4d640f027c41f91d7f5178d998c825b1168691d
PythonHubStudio <76648932+PythonHubStudio@users.noreply.github.com>
2024-01-28 14:30:11 +0200

    Delete lesson_6-prepared_template_for_review directory

--- lesson_6-prepared_template_for_review/common/bot_cmds_list.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import BotCommand
-
-
-private = [
-    BotCommand(command='menu', description='Посмотреть меню'),
-    BotCommand(command='about', description='О нас'),
-    BotCommand(command='payment', description='Варианты оплаты'),
-    BotCommand(command='shipping', description='Варианты доставки'),
-]
